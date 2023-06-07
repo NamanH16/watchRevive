cfb8eee12c44239b7cfe2401d18130de43b539cd
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-06-07 12:00:00 +0530

    format components --Divider

--- client/src/components/Divider.js
+++ client/src/components/Divider.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Divider = () => {
-  return (
-    <div
-        className='w-full h-[1px] bg-gray-300 my-2'
-    ></div>
-  )
-}
+  return <div className="w-full h-[1px] bg-gray-300 my-2"></div>;
+};
 
-export default Divider
+export default Divider;
