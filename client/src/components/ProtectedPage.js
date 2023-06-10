f1b8f5e51ed0b9fde466847dea2e670151a239cc
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-06-10 12:00:00 +0530

    format components --ProtectedPage

--- client/src/components/ProtectedPage.js
+++ client/src/components/ProtectedPage.js
@@ -2,13 +2,13 @@ import React, { useEffect } from "react";
 import { message } from "antd";
 import { GetCurrentUser } from "../apicalls/users";
 import { useNavigate } from "react-router-dom";
-import wristwatchImage from "../assets/wristwatch.png"
+import wristwatchImage from "../assets/wristwatch.png";
 import { useDispatch, useSelector } from "react-redux";
 import { SetLoader } from "../redux/loadersSlice";
 import { SetUser } from "../redux/usersSlice";
 
 function ProtectedPage({ children }) {
-  const { user } = useSelector((state)=> state.users);
+  const { user } = useSelector((state) => state.users);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const validateToken = async () => {
@@ -17,7 +17,7 @@ function ProtectedPage({ children }) {
       const response = await GetCurrentUser();
       dispatch(SetLoader(false));
       if (response.success) {
-        dispatch(SetUser(response.data))
+        dispatch(SetUser(response.data));
       } else {
         navigate("/login");
         message.error(response.message);
@@ -41,27 +41,34 @@ function ProtectedPage({ children }) {
       <div>
         {/* Header */}
         <div className="flex justify-between items-center bg-primary p-5">
-          <h1 className="text-2xl text-white cursor-pointer" onClick={()=>navigate("/")}>
-          WATCH <img src={wristwatchImage} alt="Wristwatch" className="h-8" />{" "}
-          REVIVE
+          <h1
+            className="text-2xl text-white cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            WATCH <img src={wristwatchImage} alt="Wristwatch" className="h-8" />{" "}
+            REVIVE
           </h1>
 
           <div className="bg-white py-2 px-5 rounded flex gap-1 items-center">
-          <i className="ri-user-3-line"></i>
-            <span className="underline cursor-pointer uppercase" onClick={()=> {
-              if(user.role==='user'){
-                navigate("/profile");
-              }else{
-                navigate("/admin");
-              }
-            }}>
+            <i className="ri-user-3-line"></i>
+            <span
+              className="underline cursor-pointer uppercase"
+              onClick={() => {
+                if (user.role === "user") {
+                  navigate("/profile");
+                } else {
+                  navigate("/admin");
+                }
+              }}
+            >
               {user.name}
             </span>
-            <i className="ri-logout-circle-r-line ml-6 cursor-pointer" 
-                onClick={()=>{
-                  localStorage.removeItem("token");
-                  navigate("/login");
-                }}
+            <i
+              className="ri-logout-circle-r-line ml-6 cursor-pointer"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/login");
+              }}
             ></i>
           </div>
         </div>
